9567d9a8fce60930bdaf5f0fce44b398c1384f88
Kale Gillespie <kalelgillespie@gmail.com>
2023-01-12 08:01:39 -0600

    signin

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss/forms').Config} */
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+   // require('@tailwindcss/forms')
+  ],
 }
